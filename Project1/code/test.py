7c5d2ea1e7049d9351c23dec0c8ffd8a1e1ec06c
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-02-16 11:50:43 -0500

    testing against the 15-puzzle problem

--- Project1/code/test.py
+++ Project1/code/test.py
@@ -8,14 +8,21 @@ from custom.traverse import get_children, bfs, backtrack
 if __name__ == "__main__":
 
     # define our goal node and start node
-    goal_node = make_node([*range(9)])
-    start_node = make_node([1,0,2,3,4,5,6,7,8])
+    goal = [*range(16)]
+    goal_node = make_node(goal)
+    
+    a = goal[0:2]
+    a.reverse()
+    b = goal
+    b[0:2] = a
+
+    start_node = make_node(b)
 
     # initialize our tree
     tree = Tree(goal_node)
 
     # build out the brute force search of all paths
-    tree = bfs(start_node, tree, verbose=False)
+    tree = bfs(start_node, tree, verbose=True)
 
     # find the optimal successful path
     if not tree.successes:
