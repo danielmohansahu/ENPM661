4f181d7d7e95c030a0827d70c803aa28896032aa
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-02-22 19:29:21 -0500

    Added optional --start input argument

--- Project1/code/solve.py
+++ Project1/code/solve.py
@@ -11,8 +11,22 @@ def parse_args():
     """Parse command line args
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--size", type=int, default=9, help="Size of puzzle to solve.")
-    return parser.parse_args()
+    parser.add_argument("-n", "--number", type=int, default=9, help="Size of puzzle to solve.")
+    parser.add_argument("-r", "--random", type=bool, default=True, help="Solve a random puzzle.")
+    parser.add_argument("-s", "--start", nargs="+", type=int, required=False, help="Initial start node (as a list).")
+
+    # parse args
+    args = parser.parse_args()
+
+    # determine run mode based on input flags
+    if args.start:
+        # if we're given an argument list, ignore other args
+        args.size = len(args.start)
+        args.random = False
+    elif not args.random:
+        raise RuntimeError("Cannot run with --random False and no --start node given.")
+
+    return args
 
 if __name__ == "__main__":
     # parse arguments
@@ -24,9 +38,16 @@ if __name__ == "__main__":
     print("Goal node: {}".format(goal_node))
 
     # define a random start node
-    start_node = make_node(np.random.permutation(goal_node.state.flatten()))
-    while not start_node.solveable():
+    if args.start:
+        # use the user input node
+        start_node = make_node(args.start)
+        if not start_node.solveable():
+            raise RuntimeError("Given node {} is not solveable.".format(args.start))       
+    else:
+        # make a random starting node
         start_node = make_node(np.random.permutation(goal_node.state.flatten()))
+        while not start_node.solveable():
+            start_node = make_node(np.random.permutation(goal_node.state.flatten()))
     print("Start node: {}".format(start_node))
 
     # initialize our tree
