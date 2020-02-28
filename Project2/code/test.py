7aea704cc63f729854176d8d64c6e96945509904
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-02-28 17:56:02 -0500

    working example of graph search; core search algorithm not yet implemented

--- Project2/code/test.py
+++ Project2/code/test.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
 
-from random import sample, randrange
-from collections import defaultdict
+import sys
 import numpy as np
+from dijkstra.node import Node
+from dijkstra.map import Map
+from dijkstra.graph import Graph
 from dijkstra.dijkstra import Dijkstra
 
 if __name__ == "__main__":
-    # dummy graph (for testing)
-    g = {
-        (1,2): {
-            (2,3): {(3,4)},
-            (5,4): {(3,5)}
-        }
-    }
-    d = Dijkstra()
+
+    # start node
+    start_node = Node(np.array([19,33]))
+
+    # dummy map (for testing)
+    obstacle_map = Map(xbounds=[0,300],ybounds=[0,200])
+
+    # generate graph
+    sys.setrecursionlimit(1500)
+    graph = Graph(obstacle_map, start_node)
+    
+    
+    # d = Dijkstra()
 
     import code
     code.interact(local=locals())
