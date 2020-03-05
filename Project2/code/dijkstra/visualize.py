ed316f34b4d96296723905427b5a0a08964df1a1
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-03-04 19:23:42 -0500

    Further attempts at improving visualization; still not great.

--- Project2/code/dijkstra/visualize.py
+++ Project2/code/dijkstra/visualize.py
@@ -6,7 +6,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-def plot_path(map_, path):
+def plot_path(map_, path, show=False):
     """Visualize the given optimal path in the given map_.
 
     Args:
@@ -27,44 +27,65 @@ def plot_path(map_, path):
     Y = [n.vertices[1] for n in path]
 
     plt.plot(X,Y)
-    plt.show()
+    if show:
+        plt.show()
 
 
 class ExplorationVisualizer:
     """Helper class to generate visualization of node exploration.
     """
-    def __init__(self, map_, nodes, costs):
+    def __init__(self, map_, nodes, costs, optimal):
         # search variables
         self.map_ = map_
         self.nodes = nodes
         self.costs = costs
+        self.optimal = optimal
 
         # visualization variables
         self.fig, self.ax = plt.subplots()
-        self.xdata, self.ydata = [],[]
+        self.map_xdata, self.map_ydata = [],[]
 
     def plot(self):
         """Actually perform the visualization.
         """
         ani = FuncAnimation(
                 self.fig, 
-                self._update, 
-                frames=len(self.nodes), 
-                init_func=self._init, 
+                self._update,
+                interval=0,
+                frames=range(len(self.nodes)),
                 blit=True)
         plt.show()
     
-    def _init(self):
-        self.ln, = self.map_.plot()
-        plt.plot(*self.nodes[0].vertices,'*b')
-        plt.text(*self.nodes[0].vertices,"START")
-        self.ln.set_marker('.')
-        self.ln.set_linestyle(' ')
-        return self.ln,
-    
     def _update(self,frame):
-        self.xdata.append(self.nodes[frame].vertices[0]) 
-        self.ydata.append(self.nodes[frame].vertices[1]) 
-        self.ln.set_data(self.xdata, self.ydata)
+        # perform initialization
+        if frame == 0:
+            # draw map background
+            self.ln, = self.map_.plot()
+
+            # draw start node
+            plt.plot(*self.nodes[0].vertices,'*b')
+            plt.text(*self.nodes[0].vertices,"START")
+
+            # reset X/Y data and plot 
+            self.map_xdata = self.ln.get_xdata()
+            self.map_ydata = self.ln.get_ydata()
+            
+            # set data
+            self.ln.set_marker("*")
+            self.ln.set_markersize(4)
+            self.ln.set_linestyle(" ")
+
+        # update X/Y data and plot 
+        new_x = self.map_xdata + [self.nodes[frame].vertices[0]]
+        new_y = self.map_ydata + [self.nodes[frame].vertices[1]]
+        self.ln.set_data(new_x,new_y)
+
+        # if this is the goal node also write that text
+        #  and plot optimal path
+        if frame == len(self.nodes)-1:
+            plot_path(self.map_, self.optimal)
+
+            # wait for user input
+            plt.waitforbuttonpress()
+        
         return self.ln,
-    
