e50e57dc1039e0f3673ee80cf815fd460a599fa4
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-03-11 18:54:51 -0400

    Initial commit of Project3 (copied from Project2)

---
+++ Project3/code/custom/graph.py
@@ -0,0 +1,70 @@
+"""Class representing the Graph of Nodes (derived via BFS)
+"""
+
+from collections import defaultdict
+from .node import Node
+
+class Graph:
+    """Iteratively build and store a graph of Nodes from a given start.
+    """
+    def __init__(self, map_, start_node, buffer_=0):
+        # map of obstacle space; used to in collision detection
+        self.map_ = map_
+
+        # buffer size to use (i.e. robot radius
+        self.buffer_ = buffer_
+        
+        # sanity check input
+        if not isinstance(start_node, Node):
+            raise ValueError("Start node given to Graph isn't a Node object.")
+
+        # nodes: dict of explored nodes (key: value) -> (hash: node)
+        # tree: hierarchical relationships between nodes (parent: children)
+        self.nodes, self.tree = self.construct(start_node)
+
+    def construct(self, start_node):
+        """Build out the set of Nodes, starting from our start node.
+        """
+        tree = defaultdict(dict)
+        nodes = {}
+        
+        current_nodes = [start_node] 
+        while len(current_nodes) != 0:
+            new_nodes = []
+            for node in current_nodes:
+                node_hash = hash(node)
+                
+                # invalid node (obstacle or outside bounds)
+                if not self.map_.is_valid(node.vertices, self.buffer_):
+                    continue
+                
+                # add valid nodes to our tree
+                if node.parent:
+                    tree[hash(node.parent)][node_hash] = node.cost2come - node.parent.cost2come
+               
+                # we've already explored this node
+                if node_hash in nodes.keys():
+                    continue
+
+                # valid node; add it to our visited nodes
+                nodes[node_hash] = node
+                children = node.get_children()
+                
+                # add children to be processed next round
+                new_nodes += children
+
+            current_nodes = new_nodes
+        return nodes, tree
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Project3/code/custom/visualize.py
@@ -0,0 +1,96 @@
+"""Visualization Tools
+"""
+
+from .node import Node
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+def plot_path(map_, path, show=False):
+    """Visualize the given optimal path in the given map_.
+
+    Args:
+        map_: The workspace map.
+        path: A list of Node objects.
+    """
+    # plot map
+    map_.plot()
+
+    # add start end end nodes (with text)
+    plt.plot(*path[0].vertices,'*b')
+    plt.text(*path[0].vertices,"START")
+    plt.plot(*path[-1].vertices,'*r')
+    plt.text(*path[-1].vertices,"GOAL")
+
+    # convert node to X,Y list
+    X = [n.vertices[0] for n in path]
+    Y = [n.vertices[1] for n in path]
+
+    plt.plot(X,Y)
+    if show:
+        plt.show()
+
+
+class ExplorationVisualizer:
+    """Helper class to generate visualization of node exploration.
+    """
+    def __init__(self, map_, nodes, costs, optimal):
+        # search variables
+        self.map_ = map_
+        self.nodes = nodes
+        self.costs = costs
+        self.optimal = optimal
+
+        # visualization variables
+        self.fig,self.ax = self.map_.plot()
+        self.ln, = plt.plot([],[],' .b')
+        self.map_xdata, self.map_ydata = [],[]
+        self.stop_running=False
+
+    def plot(self):
+        """Actually perform the visualization.
+        """
+        ani = FuncAnimation(
+                self.fig, 
+                self._update,
+                interval=1,
+                init_func=self._init,
+                frames=range(len(self.nodes)),
+                blit=True)
+        plt.show()
+   
+    def _init(self):
+        # add start and end node (with text)
+        plt.plot(*self.nodes[0].vertices,'*b')
+        plt.text(*self.nodes[0].vertices,"START")
+        plt.plot(*self.optimal[-1].vertices,'*r')
+        plt.text(*self.optimal[-1].vertices,"GOAL")
+        return self.ln,
+
+    def _update(self,frame):
+        if self.stop_running:
+            return self.ln,
+
+        if frame==0:
+            # zero out data and start fresh
+            self.map_xdata = []
+            self.map_ydata = []
+        
+        # update X/Y data and plot 
+        self.map_xdata.append(self.nodes[frame].vertices[0])
+        self.map_ydata.append(self.nodes[frame].vertices[1])
+        self.ln.set_data(self.map_xdata, self.map_ydata)
+
+        # if this is the goal node also write that text
+        #  and plot optimal path
+        if frame == len(self.nodes)-1:
+            self.ln.set_data([],[])
+
+            # convert node to X,Y list
+            X = [n.vertices[0] for n in self.optimal]
+            Y = [n.vertices[1] for n in self.optimal]
+            self.ln, = plt.plot(X,Y)
+
+            self.stop_running=True
+        
+        return self.ln,
