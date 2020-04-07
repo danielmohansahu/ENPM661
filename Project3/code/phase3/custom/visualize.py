00d8f003fddc60ad7fd3cc5f7e6f141eb7f6a26b
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-04-07 15:48:21 -0400

    Refined visualization

--- Project3/code/phase3/custom/visualize.py
+++ Project3/code/phase3/custom/visualize.py
@@ -1,10 +1,11 @@
 """Visualization Tools
 """
 
-from .node import Node
+import sys
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation, writers
+from .node import Node
 
 def plot_path(map_, path, show=False):
     """Visualize the given optimal path in the given map_.
@@ -42,14 +43,22 @@ class ExplorationVisualizer:
 
         # visualization variables
         self.fig,self.ax = self.map_.plot()
-        self.ln = plt.quiver([],[],[],[],angles='xy')
-        self.x_data = []
-        self.y_data = []
-        self.u_data = []
-        self.v_data = []
+        self.x_data = [self.nodes[i].vertices[0] for i in range(len(self.nodes)-1)]
+        self.y_data = [self.nodes[i].vertices[1] for i in range(len(self.nodes)-1)]
+        self.u_data = [0]*len(self.x_data)
+        self.v_data = [0]*len(self.x_data)
+
+        self.ln = self.ax.quiver(
+            self.x_data,
+            self.y_data,
+            self.u_data,
+            self.v_data, 
+            color='b', 
+            angles='xy',
+            scale=10)
 
         # misc variables
-        self.max_size = 1000
+        self.max_size = 500
 
     def plot(self, save=True):
         """Actually perform the visualization.
@@ -62,7 +71,7 @@ class ExplorationVisualizer:
                 repeat_delay=10,
                 init_func=self._init,
                 frames=range(len(self.nodes)),
-                blit=True)
+                blit=False)
 
         if save and "ffmpeg" in writers.list():
             # save to a local video file
@@ -86,34 +95,21 @@ class ExplorationVisualizer:
         return self.ln,
 
     def _update(self,frame):
-        
-        # skip the first node (it has no parent)
-        if not self.nodes[frame].parent:
-            return self.ln,
+        sys.stdout.write('\r')
+        sys.stdout.write("[%-20s] %d%%" % ('='*int(20*frame/len(self.nodes)), int(100*frame/len(self.nodes))))
+        sys.stdout.flush()
 
-        # update X/Y data and plot 
-        self.x_data.append(self.nodes[frame].parent.vertices[0])
-        self.y_data.append(self.nodes[frame].parent.vertices[1])
-        self.u_data.append(self.nodes[frame].vertices[0]-self.x_data[-1])
-        self.v_data.append(self.nodes[frame].vertices[1]-self.y_data[-1])
-        
-        # only keep the last N values 
-        if len(self.x_data) > self.max_size:
-            self.x_data.pop(0)
-            self.y_data.pop(0)
-            self.u_data.pop(0)
-            self.v_data.pop(0)
-
-        # set updated data
-        self.ln = plt.quiver(self.x_data, self.y_data, self.u_data, self.v_data, angles='xy')
-
-        # if this is the goal node also write that text
-        #  and plot optimal path
         if frame == len(self.nodes)-1:
-
             # convert node to X,Y list
             X = [n.vertices[0] for n in self.optimal]
             Y = [n.vertices[1] for n in self.optimal]
             self.ln, = plt.plot(X,Y)
+            return self.ln,
+
+        # update X/Y data and plot 
+        self.u_data[frame] = self.nodes[frame+1].vertices[0]-self.nodes[frame].vertices[0]
+        self.v_data[frame] = self.nodes[frame+1].vertices[1]-self.nodes[frame].vertices[1]
         
+        # set updated data
+        self.ln.set_UVC(self.u_data, self.v_data)
         return self.ln,
