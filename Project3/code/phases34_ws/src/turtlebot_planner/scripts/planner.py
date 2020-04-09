040e8fb586943893310656afe73447744ce0bbe4
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-04-09 11:44:50 -0400

    Renamed core script

--- Project3/code/phases34_ws/src/turtlebot_planner/scripts/planner.py
+++ Project3/code/phases34_ws/src/turtlebot_planner/scripts/planner.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 
 import sys
 import argparse
+import rospy
+from geometry_msgs.msg import Twist
 from turtlebot_planner.map import FinalMap
 from turtlebot_planner.node import Node, ActionSet
 from turtlebot_planner.graph import Graph
@@ -10,9 +12,9 @@ from turtlebot_planner.options import Options
 from turtlebot_planner.visualize import ExplorationVisualizer
 
 # default input args
-DEFAULT_START = [-4, -4, 60]
+DEFAULT_START = [-4, -4, 0]
 DEFAULT_GOAL = [4, 4]
-DEFAULT_RPM = [100, 100]
+DEFAULT_RPM = [1000, 200]
 DEFAULT_CLEARANCE = 0.01
 
 def parse_args():
@@ -25,14 +27,44 @@ def parse_args():
         default=DEFAULT_RPM, nargs='+', type=float, help="Input RPM")
     parser.add_argument("-c", "--clearance", 
         default=DEFAULT_CLEARANCE, type=float, help="Obstacle avoidance clearance.")
-    
-    args = parser.parse_args()
-    options = Options(args.start, args.goal, args.rpm, args.clearance)
+    parser.add_argument("-v", "--visualize", action="store_true",
+        help="Generate a video of the path planning process.")
+
+    args,_ = parser.parse_known_args()
+
+    # convert input Yaw to degrees
+    args.start[2]*=180/3.14159
+
+    options = Options(args.start, args.goal, args.rpm, args.clearance, args.visualize)
     return options
+
+def generate_trajectory(path, r, L):
+    """Convert a given path to a fixed-timestep trajectory.
+    """
+    trajectory = []
+    for node in path:
+        action = node.parent_action
+
+        # skip nodes without parent action (e.g. first)
+        if action is None:
+            continue
+        
+        msg = Twist()
+
+        # compute twist message from given actions (convert to m/s, rad/s)
+        msg.linear.x = r*(action[0]+action[1])/120.0 
+        msg.angular.z = r*(action[1]-action[0])/(60.0*L)
+        trajectory.append(msg)
+    return trajectory
 
 if __name__ == "__main__":
     # get input arguments
     options = parse_args()
+
+    # initialize as ROS node
+    rospy.init_node('planner', anonymous=True)
+    publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    sleep_time = options.timestep*60.0
 
     # generate obstacle map
     obstacle_map = FinalMap()
@@ -43,7 +75,7 @@ if __name__ == "__main__":
             options.wheel_radius,
             options.wheel_separation,
             options.timestep)
-    resolution = action_set.calc_resolution()
+    resolution = action_set.calc_resolution(options.start[2])
     print("Using graph resolution {}".format(resolution))
     Node.set_actionset(action_set)
     Node.set_resolution(resolution)
@@ -76,10 +108,27 @@ if __name__ == "__main__":
     optimal_path,_ = d.get_path()
 
     # visualize optimal path (and make video of exploration)
-    visualizer = ExplorationVisualizer(
-            obstacle_map, 
-            *d.get_exploration(True, goal_node),
-            optimal_path
-    ) 
-    visualizer.plot(False)
+    if options.visualize:
+        visualizer = ExplorationVisualizer(
+                obstacle_map, 
+                optimal_path,
+                *d.get_exploration(True, goal_node)
+        ) 
+        visualizer.plot(True)
+
+    # control to path
+    traj = generate_trajectory(optimal_path, options.wheel_radius, options.wheel_separation)
+
+    # control to plan (open loop)
+    print("Publishing desired velocity commands to /cmd_vel")
+    for msg in traj:
+        publisher.publish(msg)
+        rospy.sleep(sleep_time)
+    
+    # publish stop message at the end
+    publisher.publish(Twist())
+    print("Finished commanded velocities.")
+
+    rospy.spin()
+
 
