fb9ab883df505b0485d4354275f70dac3a45f195
Daniel M. Sahu <danielmohansahu@gmail.com>
2020-04-19 07:24:10 -0400

    Modified dockerfile

--- Project4/code/run.sh
+++ Project4/code/run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 docker run -ti --rm \
-       -e DISPLAY=$DISPLAY \
-       -v /tmp/.X11-unix:/tmp/.X11-unix \
-       -v /home/danielmohansahu/Homework/ENPM661/Project4/code/catkin_ws:/catkin_ws \
-       --net dockernet --ip 172.18.1.10 --hostname developer\
-       enpm661_project4:latest byobu
+	--env="DISPLAY" \
+	--env="QT_X11_NO_MITSHM=1" \
+	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+        -v /home/danielmohansahu/Homework/ENPM661/Project4/code/catkin_ws:/catkin_ws \
+        --net dockernet --ip 172.18.1.10 --hostname developer\
+       	enpm661_project4:latest byobu
+       	# --net host \
+       # --net dockernet --ip 172.18.1.10 --hostname developer\
+       # -e DISPLAY=$DISPLAY \
+       # -v /tmp/.X11-unix:/tmp/.X11-unix \
+       # -v /home/danielmohansahu/Homework/ENPM661/Project4/code/catkin_ws:/catkin_ws \
